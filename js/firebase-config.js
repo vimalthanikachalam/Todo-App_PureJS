64e5fb79daef4cbdabff60d1aaf089de0147c855
Vimal Thanikachalam <vimaleurakaa@gmail.com>
2021-02-18 19:21:21 +0530

    init

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCHtk_CFAmeH9Kwpzg97bKyazoKLF62KPk",
+  authDomain: "a-todo-app.firebaseapp.com",
+  projectId: "a-todo-app",
+  storageBucket: "a-todo-app.appspot.com",
+  messagingSenderId: "378487985449",
+  appId: "1:378487985449:web:fa709ac8683804bfc21338",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const task_content = document.getElementById("task_content");
+const add_note = document.getElementById("add_note");
+const database = firebase.database().ref("data");
+
+//Get input value
+const addContent = () => {
+  const value = task_content.value;
+  if (value != "") {
+    sendData(value);
+  } else {
+    alert("Input Value cannot be empty!! Please try again.");
+  }
+
+  //   sendData();
+};
+
+add_note.addEventListener("click", addContent);
+
+//Update to database
+const sendData = (value) => {
+  const list = database.child("todo_list");
+  const key = user.push().key;
+  list.update({ [key]: value }).then(console.log("Successfully Updated!"));
+};
